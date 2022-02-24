bfe220d6e3fa4c46f3383559c12780408b9b9466
Joel Bascombe <joelbascombe13@gmail.com>
2022-02-24 12:08:14 -0500

    Pratice: Approach

---
+++ Sorted_Squared_Array.js
@@ -0,0 +1,12 @@
+// Write a function that takes in a non-empty array of integers that are sorted in ascending order and returns a new array of the same length with squares of the original integers also sorted in ascending order
+function sortedSquaredArray(array) {
+  // Write your code here.
+  // create empty arr
+  // loop arr.length
+  // arr[i] * arr[i]
+  //push  arr[i] * arr[i] -> arr
+  // return arr
+  // arr = [1, 2, 3];
+
+  return [];
+}
