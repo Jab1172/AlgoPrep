7069a0195a89d68f937f3e662dbbf6c720f827ad
Joel Bascombe <joelbascombe13@gmail.com>
2022-02-24 11:57:40 -0500

    Pratice: Problem

---
+++ SumOfThree.js
@@ -0,0 +1,18 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+var twoSum = function (nums, target) {
+  //loop through nums
+  //create object for of...
+  //conditional nums[i] + nums[i+1] === target
+  //return
+  //time O(n)
+  //space 0(1)
+  //
+  //         //      0 1 2
+  //     let nums = [1,2,3]
+  //     let target = 5
+};
